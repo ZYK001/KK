8b02c13d56ed0c8c42cfd1efa329904cbf392acc
ZYK001 <15022535129@163.com>
2016-01-18 15:32:51 +0800

    aa

---
+++ KKDemo/KKDemo/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  KKDemo
+//
+//  Created by 赵英奎 on 16/1/18.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
---
+++ KKDemo/KKDemo/AViewController.m
@@ -0,0 +1,40 @@
+
+
+
+//
+//  AViewController.m
+//  KKDemo
+//
+//  Created by 赵英奎 on 16/1/18.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
